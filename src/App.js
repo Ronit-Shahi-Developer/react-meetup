cca2bcb4d880f36edfd42378e48df358391c5a0f
Ronit Shahi <ronytshahi@gmail.com>
2021-05-27 13:28:29 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Route, Switch} from "react-router-dom";
+
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favourites from "./pages/Favourites";
+import MainNavigation from './components/layout/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+      <Route path="/" exact>
+        <AllMeetups />
+      </Route>
+
+            <Route path="/new-meetup">
+        <NewMeetup />
+      </Route>
+
+      <Route path="/favourites">
+        <Favourites />
+      </Route>
+
+      
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewMeetup() {
+    return (
+        <div>
+            <h1>why</h1>
+        </div>
+    )
+}
+
+export default NewMeetup
